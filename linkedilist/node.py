df847b2b8fa9e405430aeee93ae2a1adfd77e4c9
Jayavel <jayavel1520@gmail.com>
2024-10-21 20:17:11 +0530

    SLL creation

--- linkedilist/node.py
+++ linkedilist/node.py
@@ -5,3 +5,27 @@
 #         self.data = data
 #         self.next = None
 # y = Node(6)
+
+
+# Singly linked list creation
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+class SLL:
+    def __init__(self) -> None:
+        self.head = None
+
+    def creation(self):
+        nodes = int(input("Enter the number of Nodes: "))
+        for i in range(nodes):
+            newNode = Node(int(input("Enter the Node Value: ")))
+            
+            if self.head is None:
+                self.head = newNode
+            
+            else:
+                current.next = newNode
+
+            current = newNode 
