3edc2596845e4781a3ca1e31aa00c7c7b4834dd1
Jayavel <jayavel1520@gmail.com>
2024-11-03 11:02:11 +0530

    Infix to prefix

--- stack/infix.py
+++ stack/infix.py
@@ -1,4 +1,45 @@
-def infix_to_postfix(s):
+# def infix_to_postfix(s):
+#     stack = []
+#     output = []
+#     precedence = {"+": 1,"-":1,"*":2,"/":2,"^":3}
+#     for char in s:
+#         if char.isalnum():
+#             output.append(char)
+#         elif char == "(":
+#             stack.append(char)
+#         elif char == ")":
+#             while stack and stack[-1] != "(":
+#                 output.append(stack.pop())
+#             stack.pop()
+#         else:
+#             while stack and stack[-1] != "(" and precedence[char] <= precedence[stack[-1]]:
+#                 output.append(stack.pop())
+#             stack.append(char)
+
+#     while stack:
+#         output.append(stack.pop())
+#     return "".join(output)
+# str = "a+b*(c^d-e)^(f+g*h)-i"
+# print(infix_to_postfix(str))
+
+
+
+
+# Infix to prefix
+
+
+def reverse(s):
+    reverseStr = ""
+    for char in s:
+        if char == "(":
+            reverseStr += ")"
+        elif char == ")":
+            reverseStr += "("
+        else:
+            reverseStr += char
+    return reverseStr[::-1]
+
+def infix_to_prefix(s):
     stack = []
     output = []
     precedence = {"+": 1,"-":1,"*":2,"/":2,"^":3}
@@ -18,6 +59,9 @@ def infix_to_postfix(s):
 
     while stack:
         output.append(stack.pop())
-    return "".join(output)
+    result = "".join(output)
+    return result[::-1]
+
 str = "a+b*(c^d-e)^(f+g*h)-i"
-print(infix_to_postfix(str))
+expres = reverse(str)
+print(infix_to_prefix(expres))
