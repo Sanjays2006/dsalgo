6f0d51c850e4cf785255e862686e5c18916c9d9f
Jayavel <jayavel1520@gmail.com>
2024-11-05 19:29:16 +0530

    Graph BFS normal method

--- Graph/graphBfs.py
+++ Graph/graphBfs.py
@@ -36,3 +36,28 @@
 #     graph.insert(v1,v2)
 # graph.display()
 # graph.bfs(int(input("Enter the start Node: ")))
+
+
+def bfs(graph,sn):
+    queue = [sn]
+    visited = set()
+    visited.add(sn)
+    while queue:
+        node = queue.pop(0)
+        print(node, end=" -> ")
+
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+vertex = int(input("Enter the vertex: "))
+edge = int(input("Enter the edges: "))
+adjList = [[] for _ in range(vertex)]
+
+for i in range(edge):
+    v1, v2 = map(int,input().split())
+    adjList[v1].append(v2)
+    adjList[v2].append(v1)
+
+sn = int(input("Enter the Starting Vertex: "))
+bfs(adjList,sn)
