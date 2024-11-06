d97466cf816a7e9337adb89aa50fbda7f74dfe07
Jayavel <jayavel1520@gmail.com>
2024-11-06 12:05:41 +0530

    BST

--- tree/BinarSearchTree/bst.py
+++ tree/BinarSearchTree/bst.py
@@ -1,65 +1,65 @@
 from collections import deque
-# class Node:
-#     def __init__(self,data):
-#         self.left = None
-#         self.data = data
-#         self.right = None
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.data = data
+        self.right = None
     
-# class BST:
-#     def __init__(self,data):
-#         self.root = Node(data)
+class BST:
+    def __init__(self,data):
+        self.root = Node(data)
 
-#     def insert(self,data):
-#         if self.root is None:
-#             self.root = Node(data)
-#             return
+    def insert(self,data):
+        if self.root is None:
+            self.root = Node(data)
+            return
         
-#         queue = deque([self.root])
+        queue = deque([self.root])
 
-#         while queue:
-#             current = queue.popleft()
-#             if data < current.data:
-#                 if current.left is None:
-#                     current.left = Node(data)
-#                     break
-#                 else:
-#                     queue.append(current.left)
+        while queue:
+            current = queue.popleft()
+            if data < current.data:
+                if current.left is None:
+                    current.left = Node(data)
+                    break
+                else:
+                    queue.append(current.left)
             
-#             if data > current.data:
-#                 if current.right is None:
-#                     current.right = Node(data)
-#                     break
-#                 else:
-#                     queue.append(current.right)
+            if data > current.data:
+                if current.right is None:
+                    current.right = Node(data)
+                    break
+                else:
+                    queue.append(current.right)
 
-#     def bfs(self,node):
-#         if node is None:
-#             return []
+    def bfs(self,node):
+        if node is None:
+            return []
         
-#         result = []
-#         queue = deque([node])
+        result = []
+        queue = deque([node])
 
-#         while queue:
-#             current = queue.popleft()
-#             result.append(current.data)
+        while queue:
+            current = queue.popleft()
+            result.append(current.data)
 
-#             if current.left is not None:
-#                 queue.append(current.left)
+            if current.left is not None:
+                queue.append(current.left)
 
-#             if current.right is not None:
-#                 queue.append(current.right)
+            if current.right is not None:
+                queue.append(current.right)
 
-#         return result
+        return result
 
 
-# root = int(input("Enter the Root: "))
-# bst = BST(root)
+root = int(input("Enter the Root: "))
+bst = BST(root)
 
-# Nodes = int(input("Enter the Number of Nodes: "))
-# for _ in range(Nodes):
-#     bst.insert(int(input("Enter the Node Value: ")))
+Nodes = int(input("Enter the Number of Nodes: "))
+for _ in range(Nodes):
+    bst.insert(int(input("Enter the Node Value: ")))
 
-# print(bst.bfs(bst.root))
+print(bst.bfs(bst.root))
 
 
 class Node:
@@ -107,14 +107,14 @@ class BST_Recursive:
         return result
 
 
-# Accepting input for the root node and additional nodes
-root_value = int(input("Enter the Root: "))
-bst = BST_Recursive(root_value)
+# # Accepting input for the root node and additional nodes
+# root_value = int(input("Enter the Root: "))
+# bst = BST_Recursive(root_value)
 
-nodes_count = int(input("Enter the Number of Nodes: "))
-for _ in range(nodes_count):
-    node_value = int(input("Enter the Node Value: "))
-    bst.insert(node_value)
+# nodes_count = int(input("Enter the Number of Nodes: "))
+# for _ in range(nodes_count):
+#     node_value = int(input("Enter the Node Value: "))
+#     bst.insert(node_value)
 
-# Output the BFS traversal of the tree
-print(bst.bfs(bst.root))
+# # Output the BFS traversal of the tree
+# print(bst.bfs(bst.root))
