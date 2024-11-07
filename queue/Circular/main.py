4fb696d481f4394a9b64108f68ff5ef0c6c87f52
Sanjays2006 <sakthivelsanjay5@gmail.com>
2024-11-07 13:52:49 +0530

    Deque Using array

--- queue/Circular/main.py
+++ queue/Circular/main.py
@@ -50,50 +50,108 @@
         
                     
 # Using Array
-class Queue:
-    def _init_(self, size):
-        self.queue = [None] * size
-        self.rear = -1
-        self.front = -1
-        self.size = size
+# class Queue:
+#     def _init_(self, size):
+#         self.queue = [None] * size
+#         self.rear = -1
+#         self.front = -1
+#         self.size = size
         
-    def enqueue(self, data):
+#     def enqueue(self, data):
+#         if self.front == -1 and self.rear == -1:
+#             self.front = self.rear = 0
+#             self.queue[self.rear] = data
+#         elif ((self.rear + 1) % self.size) == self.front:
+#             print("Queue is overflow")
+#             return
+#         else:
+#             self.rear = (self.rear + 1) % self.size
+#             self.queue[self.rear] = data
+            
+            
+#     def dequeue(self):
+#         if self.front == -1 and self.rear == -1:
+#             print("Queue is Empty.")
+#             return
+#         elif self.front == self.rear:
+#             poped = self.queue[self.front]
+#             self.front = self.rear = -1
+#             print(poped)
+#         else:
+#             poped = self.queue[self.front]
+#             self.front = (self.front + 1) % self.size
+#             print(poped)
+            
+#     def peek(self):
+#         if self.front == -1 and self.rear == -1:
+#             print("Queue is Empty.")
+#             return
+#         print(self.queue[self.front])
+        
+#     def display(self):
+#         if self.front == -1 and self.rear == -1:
+#             print("Queue is Empty.")
+#             return
+#         i = self.front
+#         while i != self.rear:
+#             print(self.queue[i])
+#             i = (i + 1) % self.size
+#         print(self.queue[self.rear])
+
+class Queue:
+    def __init__(self, capacity):
+        self.front = -1
+        self.rear = -1
+        self.queue = [None] * capacity
+        self.capacity = capacity
+        
+    def InsertLast(self, data):
         if self.front == -1 and self.rear == -1:
             self.front = self.rear = 0
             self.queue[self.rear] = data
-        elif ((self.rear + 1) % self.size) == self.front:
-            print("Queue is overflow")
+        elif ((self.rear + 1) % self.capacity) == self.front:
+            print("Queue is Full")
             return
         else:
-            self.rear = (self.rear + 1) % self.size
+            self.rear = (self.rear + 1) % self.capacity
             self.queue[self.rear] = data
-            
-            
-    def dequeue(self):
+    
+    def InsertFront(self, data):
         if self.front == -1 and self.rear == -1:
-            print("Queue is Empty.")
+            self.front = self.rear = 0
+            self.queue[self.rear] = data
+        elif ((self.rear + 1) % self.capacity) == self.front:
+            print("Queue is Full")
+            return
+        else:
+            self.front = ((self.front -1 + self.capacity ) % self.capacity)
+        self.queue[self.front] = data
+        
+    def DelFront(self):
+        if self.front == -1 and self.rear == -1:
+            print("Queue is empty.")
             return
         elif self.front == self.rear:
-            poped = self.queue[self.front]
+            popped = self.queue[self.front]
             self.front = self.rear = -1
-            print(poped)
-        else:
-            poped = self.queue[self.front]
-            self.front = (self.front + 1) % self.size
-            print(poped)
             
-    def peek(self):
+        else:
+            popped = self.queue[self.front]
+            self.front = (self.front + 1) % self.capacity
+        return popped
+    def DelLast(self):
         if self.front == -1 and self.rear == -1:
-            print("Queue is Empty.")
+            print("Queue is empty.")
             return
-        print(self.queue[self.front])
+        elif self.front == self.rear:
+            popped = self.queue[self.rear]
+            self.front = self.rear = -1
+        else:
+            popped = self.queue[self.rear]
+            self.rear = (self.rear - 1 + self.capacity) % self.capacity
+        return  popped
         
-    def display(self):
-        if self.front == -1 and self.rear == -1:
-            print("Queue is Empty.")
-            return
-        i = self.front
-        while i != self.rear:
-            print(self.queue[i])
-            i = (i + 1) % self.size
-        print(self.queue[self.rear])
+        
+        
+        
+    
